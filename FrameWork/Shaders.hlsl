339770334e1b9e09ce524537186fd9c9e03110b0
Choe Hyeon Ho <chlgusghw252@naver.com>
2024-12-29 03:51:15 +0900

    20141228
    DEBUG LOG

--- FrameWork/Shaders.hlsl
+++ FrameWork/Shaders.hlsl
@@ -67,7 +67,7 @@ float4 PSFbxSkinnedModel(VS_FBX_MODEL_OUTPUT input) : SV_TARGET
     
     if (sample.a == 0.0f)
     {
-        return float4(1.0f, 0.0f, 0.0f, 1.0f); // ALPHA is 0 -> Red
+        return float4(1.0f, 0.0f, 1.0f, 1.0f); // ALPHA is 0 -> Red
     }
     
     if (uv.x < 0 || uv.x > 1 || uv.y < 0 || uv.y > 1)
